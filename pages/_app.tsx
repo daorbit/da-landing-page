a8f28c68fca45d2d1c4c969f723f8ca9478c9797
Ajay <ajay.goswami@coderower.com>
2025-10-19 20:27:26 +0530

    feat(analytics): add Google Tag Manager scripts for enhanced tracking

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -67,6 +67,18 @@ export default function App({ Component, pageProps }: AppProps) {
           `,
           }}
         />
+
+        <script async src="https://www.googletagmanager.com/gtag/js?id=G-T2SL9F9Y2D"></script>
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+              window.dataLayer = window.dataLayer || [];
+              function gtag(){dataLayer.push(arguments);}
+              gtag('js', new Date());
+              gtag('config', 'G-T2SL9F9Y2D');
+            `,
+          }}
+        />
       </Head>
       <Component {...pageProps} />
       <CookieConsentBanner />
